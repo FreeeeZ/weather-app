69e9302a30fbc1b442dd128daa66cb62f6e6c14b
Vladislav Shell <shell-vladislav@mail.ru>
2023-02-19 02:49:19 +0700

    Fix markup

--- src/App.tsx
+++ src/App.tsx
@@ -18,9 +18,11 @@ export const App: FC = (): ReactElement => {
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src="/vite.svg" className="logo vite" alt="Vite logo" />
         </a>
+
         <a href="https://reactjs.org" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+
         <a href="https://www.typescriptlang.org" target="_blank" rel="noreferrer">
           <img src={typeScriptLogo} className="logo typescript" alt="TypeScript logo" />
         </a>
